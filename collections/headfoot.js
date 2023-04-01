8192227ac71699b1f9fc64c7b2b1960e06d53fee
F-U-N-B-O-X <129484726+F-U-N-B-O-X@users.noreply.github.com>
2023-04-01 02:09:11 -0700

    Update headfoot.js

--- collections/headfoot.js
+++ collections/headfoot.js
@@ -1,6 +1,6 @@
 const pageHead = document.getElementById('pageHead');
 
 function createHead(){
-  pageHead.innerHTML = "<img src='../images/funboxLogoGif.gif' class='headerImage">';
+  pageHead.innerHTML = "<img src='../images/funboxLogoGif.gif' class='headerImage'>';
   pageHead.innerHTML = "<a class='headlink'>HOME</a>&emsp;<a class='headlink'>ABOUT</a>&emsp;<a class='headlink'>BUY TICKETS</a>";
 }
